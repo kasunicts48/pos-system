74ef48f2c873271569d485c83ec674df1da23124
kasunlunatech <kasundananjayaweerasooriya@gmail.com>
2023-05-04 20:56:37 +0530

    created login and added dashboard layout

---
+++ src/components/dashboardLayout/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+const DashboardLayout = () => {
+  return <>
+    <div ></div>
+  </>;
+};
+
+export default DashboardLayout;
--- src/components/login/index.js
+++ src/components/login/index.js
@@ -1,16 +1,24 @@
 import React, { useState } from "react";
 import styles from "./index.module.css";
 import Image from "next/image";
+import { useRouter } from "next/router";
 
 // material UI
 import Box from "@mui/material/Box";
 import Grid from "@mui/material/Grid";
 
-const loginCredential = {};
+const loginCredential = {
+  username: "admin@gmail.com",
+  password: "123456",
+};
 
 const Login = () => {
+  const router = useRouter();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [errorShow, setErrorShow] = useState(false);
+
+  // need to add check email regex
 
   const handleEmailChange = (event) => {
     setEmail(event.target.value);
@@ -24,8 +32,17 @@ const Login = () => {
     // Handle login logic here
     console.log("email", email);
     console.log("password", password);
-    setEmail("");
-    setPassword("");
+    if (
+      email === loginCredential.username &&
+      password === loginCredential.password
+    ) {
+      setErrorShow(false);
+      router.push("/dashboard");
+      setEmail("");
+      setPassword("");
+    } else {
+      setErrorShow(true);
+    }
   };
 
   return (
@@ -47,19 +64,36 @@ const Login = () => {
               <div className={styles.loginDiv}>
                 <h2>Login</h2>
                 <div className={styles.inputFiled}>
-                  <label>E - mail</label>
+                  <label>
+                    E - mail{" "}
+                    <span
+                      className={errorShow ? styles.error : styles.errorHide}
+                    >
+                      Please enter correct email
+                    </span>{" "}
+                  </label>
                   <input
                     value={email}
                     type="text"
+                    name="username"
                     placeholder="Enter your Email"
                     onChange={handleEmailChange}
                   />
                 </div>
                 <div className={styles.inputFiled}>
-                  <label>Password</label>
+                  <label>
+                    Password{" "}
+                    <span
+                      className={errorShow ? styles.error : styles.errorHide}
+                    >
+                      Please enter correct password
+                    </span>
+                  </label>
+
                   <input
                     value={password}
                     type="password"
+                    name="password"
                     placeholder="Enter your Password"
                     onChange={handlePasswordChange}
                   />
