79eb8524d6a821a3988dc3208ff8672a795990f4
kasunlunatech <kasundananjayaweerasooriya@gmail.com>
2023-05-07 18:33:31 +0530

    add grn page added

--- src/components/dashboard/dashboardSingleCard/index.js
+++ src/components/dashboard/dashboardSingleCard/index.js
@@ -1,7 +1,16 @@
 import React from "react";
+import styles from "./index.module.css";
 
-const DashboardSingleCard = () => {
-  return <div>DashboardSingleCard</div>;
+const DashboardSingleCard = ({ name, amount, icon }) => {
+  return (
+    <>
+      <div className={styles.mainDiv}>
+        <div>{icon}</div>
+        <div className={styles.name}>{name}</div>
+        <div className={styles.amount}>{amount}</div>
+      </div>
+    </>
+  );
 };
 
 export default DashboardSingleCard;
--- src/components/dashboard/index.js
+++ src/components/dashboard/index.js
@@ -2,17 +2,43 @@ import React from "react";
 import styles from "./index.module.css";
 import DashboardSingleCard from "./dashboardSingleCard";
 
+const dashboardCardDetails = [
+  {
+    name: "Today’s Sales",
+    amount: 35000,
+    icon: ""
+  },
+  {
+    name: "Today’s revenue",
+    amount: 35000,
+    icon: ""
+  },
+  {
+    name: "Today’s Customers",
+    amount: 100,
+    icon: ""
+  },
+  {
+    name: "Today’s  total orders",
+    amount: 100,
+    icon: ""
+  },
+];
+
 const Dashboard = () => {
   return (
     <>
       <div className={styles.mainDiv}>
         <div className={styles.componentDiv}>
           <h2>Dashboard</h2>
-          <div className={styles.dashboardCardList} >
-            <DashboardSingleCard />
-            <DashboardSingleCard />
-            <DashboardSingleCard />
-            <DashboardSingleCard />
+          <div className={styles.dashboardCardList}>
+            {dashboardCardDetails.map((item,key) => {
+              return (
+                <DashboardSingleCard key={key} name={item.name} amount={item.amount} icon={item.icon} />
+              )
+            })}
+           
+           
           </div>
         </div>
       </div>
